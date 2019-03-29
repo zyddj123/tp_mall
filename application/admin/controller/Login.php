9bde0d740fc06e699247912cc5677f0c8f6d6481
zyddj123 <zyddj123@163.com>
2019-03-29 14:56:51 +0800

    login

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -1,15 +1,45 @@
 <?php
+
 namespace app\admin\controller;
 
 use think\Controller;
-use think\Db;
-use think\Request;
 use think\View;
+// use think\Request
 
 class Login extends Controller
 {
     public function login()
     {
         return view('login');
+    }
+
+    public function doLogin()
+    {
+        $jsonp = input('get.callback'); //get接收jsonp自动生成的函数名
+        $uname = input('get.uname');
+        $upwd = input('get.upwd');
+        $data = db('admin')->where('uname',$uname)->find();
+        $ret = [
+            'sta'=>0,
+            'mes'=>''
+        ];
+        if(empty($data)){
+            $ret['sta'] = 2;
+            $ret['mes'] = '用户不存在';
+        }else{
+            if($data['upwd']===md5($upwd)){
+                if($data['status']===1){
+                    $ret['sta'] = 1;
+                    $ret['mes'] = '登录成功';
+                }else{
+                    $ret['sta'] = 4;
+                    $ret['mes'] = '您已被冻结，请联系管理员';
+                }
+            }else{
+                $ret['sta'] = 3;
+                $ret['mes'] = '密码错误';
+            }
+        }
+        echo $jsonp.'('.json_encode($ret).')'; //jsonp函数名包裹json数据
     }
 }
